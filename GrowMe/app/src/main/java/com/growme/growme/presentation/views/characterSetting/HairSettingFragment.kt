7ab7f02f621041f50e360f19972c8a363020826b
HamBeomJoon <hbj0802@naver.com>
2024-07-31 14:44:38 +0900

    [setting] remove unused import

--- GrowMe/app/src/main/java/com/growme/growme/presentation/views/characterSetting/HairSettingFragment.kt
+++ GrowMe/app/src/main/java/com/growme/growme/presentation/views/characterSetting/HairSettingFragment.kt
@@ -1,3 +1,5 @@
+package com.growme.growme.presentation.views.characterSetting
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
--- GrowMe/app/src/main/java/com/growme/growme/presentation/views/characterSetting/SkinSettingFragment.kt
+++ GrowMe/app/src/main/java/com/growme/growme/presentation/views/characterSetting/SkinSettingFragment.kt
@@ -1,10 +1,10 @@
 package com.growme.growme.presentation.views.characterSetting
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.growme.growme.R
 import com.growme.growme.databinding.FragmentSkinSettingBinding
 
