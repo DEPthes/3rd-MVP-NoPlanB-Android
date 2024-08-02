366366a41e0321d10969c0902b8856135f01b627
shong9124 <shifty393@gmail.com>
2024-08-02 17:13:08 +0900

    fix/character-setting-name
    이름 부분 캐릭터 수정 및 화면 전환 추가

--- GrowMe/app/src/main/java/com/growme/growme/presentation/views/characterSetting/NameSettingFragment.kt
+++ GrowMe/app/src/main/java/com/growme/growme/presentation/views/characterSetting/NameSettingFragment.kt
@@ -1,5 +1,6 @@
 package com.growme.growme.presentation.views.characterSetting
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,10 +12,22 @@ class NameSettingFragment : Fragment() {
     private val binding by lazy {
         FragmentNameSettingBinding.inflate(layoutInflater)
     }
+    lateinit var characterSettingActivity: CharacterSettingActivity
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding.btnSubmit.setOnClickListener {
+            characterSettingActivity.replaceFragment(SettingCompleteFragment(), true)
+        }
+        binding.btnBack.setOnClickListener {
+            characterSettingActivity.replaceFragment(ClothesSettingFragment(), true)
+        }
+
         return binding.root
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is CharacterSettingActivity) characterSettingActivity = context
     }
 }
