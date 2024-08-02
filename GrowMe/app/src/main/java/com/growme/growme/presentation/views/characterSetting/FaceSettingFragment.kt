069ae70726573ff1918595c692c2484fa8fae806
shong9124 <shifty393@gmail.com>
2024-08-02 15:04:19 +0900

    fix/character-setting
    표정 수정

--- GrowMe/app/src/main/java/com/growme/growme/presentation/views/characterSetting/FaceSettingFragment.kt
+++ GrowMe/app/src/main/java/com/growme/growme/presentation/views/characterSetting/FaceSettingFragment.kt
@@ -1,5 +1,6 @@
 package com.growme.growme.presentation.views.characterSetting
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ class FaceSettingFragment : Fragment() {
     private val binding by lazy {
         FragmentFaceSettingBinding.inflate(layoutInflater)
     }
+    lateinit var characterSettingActivity: CharacterSettingActivity
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,18 +22,31 @@ class FaceSettingFragment : Fragment() {
             binding.btnFace1.setBackgroundResource(R.drawable.btn_mini_selected)
             binding.btnFace2.setBackgroundResource(R.drawable.btn_mini_default)
             binding.btnFace3.setBackgroundResource(R.drawable.btn_mini_default)
+            binding.layerFace.setBackgroundResource(R.drawable.face_1)
         }
         binding.btnFace2.setOnClickListener {
             binding.btnFace1.setBackgroundResource(R.drawable.btn_mini_default)
             binding.btnFace2.setBackgroundResource(R.drawable.btn_mini_selected)
             binding.btnFace3.setBackgroundResource(R.drawable.btn_mini_default)
+            binding.layerFace.setBackgroundResource(R.drawable.face_2)
         }
         binding.btnFace3.setOnClickListener {
             binding.btnFace1.setBackgroundResource(R.drawable.btn_mini_default)
             binding.btnFace2.setBackgroundResource(R.drawable.btn_mini_default)
             binding.btnFace3.setBackgroundResource(R.drawable.btn_mini_selected)
+            binding.layerFace.setBackgroundResource(R.drawable.face_3)
+        }
+        binding.btnNext.setOnClickListener {
+            characterSettingActivity.replaceFragment(HairSettingFragment(), true)
+        }
+        binding.btnBack.setOnClickListener {
+            characterSettingActivity.replaceFragment(SkinSettingFragment(), false)
         }
 
         return binding.root
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is CharacterSettingActivity) characterSettingActivity = context
     }
 }
