8c637d953243a0810446656f15aebc4fd3e673c3
HamBeomJoon <hbj0802@naver.com>
2024-08-13 14:05:07 +0900

    [setting] dataStore implement

--- GrowMe/app/build.gradle.kts
+++ GrowMe/app/build.gradle.kts
@@ -86,4 +86,7 @@ dependencies {
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // DataStore
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
