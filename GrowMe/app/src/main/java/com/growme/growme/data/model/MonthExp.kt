7232cc6a40aaafeaac37ef22bfd270ca7a052876
HamBeomJoon <hbj0802@naver.com>
2024-08-21 18:59:24 +0900

    [feat] dummy data 삭제

--- GrowMe/app/src/main/java/com/growme/growme/data/model/MonthExp.kt
+++
@@ -1,7 +0,0 @@
-package com.growme.growme.data.model
-
-// 각 달마다 날짜와, 그 날짜에 얻은 경험치 정보 (캘린더 페이지에서 사용)
-data class MonthExp(
-    val date: String,
-    val exp: Int
-)
--- GrowMe/app/src/main/java/com/growme/growme/data/model/MyInfo.kt
+++
@@ -1,7 +0,0 @@
-package com.growme.growme.data.model
-
-data class MyInfo(
-    val nickname: String,
-    val level: Int,
-    val exp: Int
-)
